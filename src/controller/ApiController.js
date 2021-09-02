47d897576c0c777816df38d5c5a2c7dff3643c12
nicocaputocai <nicolas_caputo@hotmail.com>
2021-09-02 20:44:48 -0300

    agregado al access point rutas remotas

--- src/controller/ApiController.js
+++ src/controller/ApiController.js
@@ -1,12 +1,23 @@
 module.exports = {
     index: function(req,res){
         return res.status(200).json({
-            bomberos: 'http://localhost:4000/api/bomberos',
-            clubes: 'http://localhost:4000/api/clubes',
-            Educacion: 'http://localhost:4000/api/educacion',
-            salud: 'http://localhost:4000/api/salud',
-            seguridad: 'http://localhost:4000/api/seguridad',
-            transporte:'http://localhost:4000/api/transporte'
+            local:{
+                bomberos: 'http://localhost:4000/api/bomberos',
+                clubes: 'http://localhost:4000/api/clubes',
+                Educacion: 'http://localhost:4000/api/educacion',
+                salud: 'http://localhost:4000/api/salud',
+                seguridad: 'http://localhost:4000/api/seguridad',
+                transporte:'http://localhost:4000/api/transporte'
+            },
+            remote:{
+                bomberos: 'https://lanusinteligente.divisioncode.com.ar/api/bomberos',
+                clubes: 'https://lanusinteligente.divisioncode.com.ar/api/clubes',
+                Educacion: 'https://lanusinteligente.divisioncode.com.ar/api/educacion',
+                salud: 'https://lanusinteligente.divisioncode.com.ar/api/salud',
+                seguridad: 'https://lanusinteligente.divisioncode.com.ar/api/seguridad',
+                transporte:'https://lanusinteligente.divisioncode.com.ar/api/transporte'
+            }
+           
         })
     }
 }
