79ea5ac1c7c15523248e8074c49dc4ef4d99ea95
nicocaputocai <nicolas_caputo@hotmail.com>
2021-09-06 10:12:35 -0300

    Comenté las lineas de las rutas locales, ya que están funcionando las remotas

--- src/controller/ApiController.js
+++ src/controller/ApiController.js
@@ -1,31 +1,31 @@
 module.exports = {
     index: function(req,res){
         return res.status(200).json({
-            local:{
-            //     bomberos:{
-            //         vista: 'http://localhost:4000/api/bomberos', 
-            //         documentacion:'http://localhost:4000/api/bomberos/doc'
+            // local:{
+            // //     bomberos:{
+            // //         vista: 'http://localhost:4000/api/bomberos', 
+            // //         documentacion:'http://localhost:4000/api/bomberos/doc'
+            // // },
+            //     clubes:{
+            //         vista: 'http://localhost:4000/api/clubes',
+            //         documentacion:'http://localhost:4000/api/clubes/doc'},
+            //     Educacion: {
+            //         vista:'http://localhost:4000/api/educacion',
+            //         documentacion:'http://localhost:4000/api/educacion/doc'
             // },
-                clubes:{
-                    vista: 'http://localhost:4000/api/clubes',
-                    documentacion:'http://localhost:4000/api/clubes/doc'},
-                Educacion: {
-                    vista:'http://localhost:4000/api/educacion',
-                    documentacion:'http://localhost:4000/api/educacion/doc'
-            },
-                salud:{
-                    vista: 'http://localhost:4000/api/salud',
-                    documentacion:'http://localhost:4000/api/salud/doc'
-                },
-                seguridad:{ 
-                    vista:'http://localhost:4000/api/seguridad',
-                    documentacion:'http://localhost:4000/api/seguridad/doc'
-            },
-                transporte:{
-                    vista:'http://localhost:4000/api/transporte',
-                    documentacion:'http://localhost:4000/api/transporte/doc'
-                }
-            },
+            //     salud:{
+            //         vista: 'http://localhost:4000/api/salud',
+            //         documentacion:'http://localhost:4000/api/salud/doc'
+            //     },
+            //     seguridad:{ 
+            //         vista:'http://localhost:4000/api/seguridad',
+            //         documentacion:'http://localhost:4000/api/seguridad/doc'
+            // },
+            //     transporte:{
+            //         vista:'http://localhost:4000/api/transporte',
+            //         documentacion:'http://localhost:4000/api/transporte/doc'
+            //     }
+            // },
             remote:{
                 clubes:{
                     vista: 'https://lanusinteligente.divisioncode.com.ar/api/clubes',
