1dfef5b75160c067e7ce5f4df04bcc5c384c69ef
nicocaputocai <nicolas_caputo@hotmail.com>
2021-09-20 21:09:29 -0300

    paquete para ver entradas y salidas en log instalado

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+let log4js = require('log4js');
 const App = express();
 
 const cors = require('cors');
@@ -14,6 +14,20 @@ const Education = require('./routes/education');
 const Health = require('./routes/health');
 const Security = require('./routes/security');
 const Transport = require('./routes/transport');
+
+    // Paquete de registro de errores
+
+    log4js.configure({
+        appenders: {
+            out: { type: 'console' },
+            app: { type: 'file', filename: 'application.log' }
+        },
+        categories: {
+            default: { appenders: [ 'out', 'app' ], level: 'info' }
+        }
+    });
+    let logger = log4js.getLogger('normal');
+    App.use(log4js.connectLogger(logger, {level: log4js.levels.INFO}));
 
 App.use(bodyParser.json())
 App.use(bodyParser.urlencoded({extended: true})) //Si va a haber formularios en este servidor tiene que ir true
