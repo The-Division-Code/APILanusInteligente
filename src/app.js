3815781c024588a08e945f9d852aa6e7826e2321
nicocaputocai <nicolas_caputo@hotmail.com>
2021-09-22 12:47:08 -0300

    eliminado paquete conflictivo en hosting

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const cors = require('cors');
 
 App.use(cors());
 
-// const Api = require('./routes/api.js');
+const Api = require('./routes/api.js');
 //const Bomberos = require('./routes/bomberos');
 const Club = require('./routes/club.js');
 const Education = require('./routes/education.js');
@@ -15,24 +15,10 @@ const Health = require('./routes/health.js');
 const Security = require('./routes/security.js');
 const Transport = require('./routes/transport.js');
 
-    // Paquete de registro de errores
-
-    log4js.configure({
-        appenders: {
-            out: { type: 'console' },
-            app: { type: 'file', filename: 'application.log' }
-        },
-        categories: {
-            default: { appenders: [ 'out', 'app' ], level: 'info' }
-        }
-    });
-    let logger = log4js.getLogger('normal');
-    App.use(log4js.connectLogger(logger, {level: log4js.levels.INFO}));
-
 App.use(bodyParser.json())
 App.use(bodyParser.urlencoded({extended: true})) //Si va a haber formularios en este servidor tiene que ir true
 App.use(express.static(__dirname + '/public'));
-// App.use('/api', Api)
+App.use('/api', Api)
 //App.use('/api/bomberos', Bomberos)
 App.use('/api/club', Club)
 App.use('/api/education', Education)
--- src/routes/Api.js
+++ src/routes/Api.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const ApiController = require('../controller/ApiController');
+const ApiController = require('../controller/ApiController.js')
 
 router.get('/', ApiController.index);
 
