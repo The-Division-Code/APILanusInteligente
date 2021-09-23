a81f2005ec8458fadc4068572385a6cbf750fb32
nicocaputocai <nicolas_caputo@hotmail.com>
2021-09-23 20:26:01 -0300

    cambio de configuración mongo

--- src/config/config.js
+++ src/config/config.js
@@ -1,4 +1,4 @@
 module.exports = {
     PORT: process.env.PORT || 4000,
-    DB: process.env.DB || 'mongodb+srv://desarrolloi:TJDz2pCs6qnNLL10@cluster0.wug6n.mongodb.net/mapaLanus?retryWrites=true&w=majority'
+    DB: process.env.DB || 'mongodb+srv://leovandone:Boeing787@cluster0.wug6n.mongodb.net/mapaLanus?retryWrites=true&w=majority'
 }
