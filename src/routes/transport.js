3da7c897595cecb3ac003c30a2354b133d3f6d25
Pablo Nicolas caputo <67564922+Nicocaputocai@users.noreply.github.com>
2022-06-24 10:58:02 -0300

    Quité ruta post

--- src/routes/transport.js
+++ src/routes/transport.js
@@ -4,6 +4,6 @@ const transportController = require('../controller/TransportController')
 
 router.get('/', transportController.getAll);
 router.get('/doc', transportController.doc)
-router.post('/create', transportController.create)
+//router.post('/create', transportController.create)
 
 module.exports = router
