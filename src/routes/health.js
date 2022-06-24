70ede499e73bf97713d125f1b5ef3909e2a79a71
Pablo Nicolas caputo <67564922+Nicocaputocai@users.noreply.github.com>
2022-06-24 10:57:33 -0300

    Quité ruta post

--- src/routes/health.js
+++ src/routes/health.js
@@ -4,6 +4,6 @@ const healthController = require('../controller/HealthController')
 
 router.get('/', healthController.getAll);
 router.get('/doc', healthController.doc)
-router.post('/create', healthController.create);
+//router.post('/create', healthController.create);
 
 module.exports = router
